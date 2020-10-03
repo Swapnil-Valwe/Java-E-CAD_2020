b6a3c0a0dc4ed46fced9ded3b6b7b86f493571c2
Swapnil-Valwe <72023075+Swapnil-Valwe@users.noreply.github.com>
2020-10-03 22:47:14 +0530

    Add files via upload

---
+++ assignment2/PyramidPattern3.java
@@ -0,0 +1,21 @@
+package assignment2;
+class PyramidPattern3
+{
+    public static void main(String args[])
+    {
+        int sp=10;
+        for (int i=1; i<=9; i++)
+        {
+            for(int k=1; k<=sp; k++)
+            {
+                System.out.print(" ");    
+            }
+            sp--;
+            for (int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
